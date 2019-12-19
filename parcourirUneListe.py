22a841a734ebf48805a892c567108d261c134f08
225junior <225juniorbrindou@gmail.com>
2019-12-19 15:21:14 +0100

    ok

---
+++ parcourirUneListe.py
@@ -0,0 +1,5 @@
+liste = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf','dix']
+for nbr in liste:
+    print(nbr)
+print('')
+print(nbr)
--- tableMulti.py
+++ tableMulti.py
@@ -1,7 +1,6 @@
 a = int(input("Nombre 1 : "))
-if a>0:
-    print(a , " Est un nombre prossitif")
-    pass
-else:
-    print(a , " Est un nombre Négatif")
-    pass
+i=0
+while i<= 10:
+    print( a," x ",i," = ", a*i)
+    i+=1
+    
