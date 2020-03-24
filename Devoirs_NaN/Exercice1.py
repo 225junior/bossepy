35118ff2b9edba304b94cbd58fcae73302f9765a
225junior <225juniorbrindou@gmail.com>
2020-03-24 15:38:55 +0000

    Exo1NaN

---
+++ Devoirs_NaN/Exercice1.py
@@ -0,0 +1,8 @@
+# Exercice 1:
+# Initialisez une variable a = 0 et b = 10. Ecrire une boucle affichant et 
+# incremantant la valeur de a tant qu'elle reste inférieur à celle de b
+a = 0
+b = 10
+while a< 10:
+    print(a)
+    a+=1
