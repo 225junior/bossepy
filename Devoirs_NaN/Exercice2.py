f1312cbfce65105bae82c5be24a4fb910be5830d
225junior <225juniorbrindou@gmail.com>
2020-03-26 02:07:03 +0000

    exo2

---
+++ Devoirs_NaN/Exercice2.py
@@ -0,0 +1,11 @@
+# Ecrire une boucle décrementant la valeur de b
+# et affichant sa valeur si elle est impaire 
+# bouclé tans que b n'est pas null
+
+# Debut
+b = 10
+while b>=0:
+    if (b%2)!=0:
+        print(b)
+    b-=1
+# Fin
