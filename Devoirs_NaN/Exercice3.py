c2f11fae175cb34c08f8049c6a2dc5fbc3289770
225junior <225juniorbrindou@gmail.com>
2020-03-26 15:54:55 +0000

    Exercice3

--- Devoirs_NaN/Exercice3.py
+++ Devoirs_NaN/Exercice3.py
@@ -1,7 +1,9 @@
 # Ecrire une saisie filtrée d'un entier dans l'intervalle fermé 1 à 10 Afficher la saisie
 
 #debut
-print('Saisissez un chiffre compris entre 1 et 10')
-nbr = int(input())
+nbr = int(input('Saisissez un chiffre compris entre 1 et 10 : '))
+while nbr<0 or nbr>10:
+    print('Erreur! Saisissez un chiffre compris entre 1 et 10 ')
+    nbr = int(input())
 print(nbr)
-#fin
+#fin230
