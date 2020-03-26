c0ee726cad89b13f73bc28910580342a21e9c98a
225junior <225juniorbrindou@gmail.com>
2020-03-26 11:33:25 +0000

    Exo5

---
+++ Devoirs_NaN/Exercice5.py
@@ -0,0 +1,7 @@
+# Afficher chaque élément d'une liste en utilisant la boucle for
+
+# Debut
+liste = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf','dix']
+for i in liste:
+    print(i)
+# fin
